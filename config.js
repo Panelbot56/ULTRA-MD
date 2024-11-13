c50b4002cd0e39636007d97b81d13c90ae9133cc
Panelbot56 <panelbot40@gmail.com>
2024-11-12 20:22:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '50935961335';
+const defaultOwner = '50938183845';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.botNumber = '50935961335'//replace your bot number here
+global.botNumber = '50938183845'//replace your bot number here
 
 global.mods = []
 global.prems = []
